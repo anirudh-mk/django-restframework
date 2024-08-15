99ff31d343e17b41d39c5246408b9f05230fbaf8
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-08-15 12:21:38 +0530

    feat(validation)

--- api/serializer.py
+++ api/serializer.py
@@ -7,6 +7,7 @@ class BookSerializer(serializers.ModelSerializer):
     author_name = serializers.CharField(source='author.name', read_only=True)
     author_country = serializers.CharField(source='author.contry', read_only=True)
     author_age = serializers.CharField(source='author.age', read_only=True)
+    author_books = serializers.SerializerMethodField()
 
     class Meta:
         model = Books
@@ -19,5 +20,17 @@ class BookSerializer(serializers.ModelSerializer):
             'author',
             "author_name",
             "author_country",
-            "author_age"
+            "author_age",
+            "author_books"
         ]
+
+    def get_author_books(self, obj):
+        author = obj.author
+        books_values = Books.objects.filter(author=author).values_list('name', flat=True)
+
+        return books_values
+
+    def validate_name(self, name):
+        if Books.objects.filter(name=name).exists():
+            raise serializers.ValidationError('book already exists')
+        return name
