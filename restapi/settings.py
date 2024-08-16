4087b59cd5ec234479559290d51ae0893a2054ed
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-08-16 18:26:36 +0530

    feat(cors headder added)

--- restapi/settings.py
+++ restapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api",
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "restapi.urls"
@@ -133,4 +136,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
-
+CORS_ORIGIN_ALLOW_ALL = True
